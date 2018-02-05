b829a9ba746e7c0b25385c6e316148d494a3146e
Lala Rukh <lalarukh.1992@outlook.com>
2018-02-05 11:52:18 +0000

    Java Game

---
+++ MainGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *MainGUI class provides a way to start the game. It creates a new instance of PlayerChoice class.
+ * @Lala Rukh
+ * @2018.01.07
+ */
+public class MainGUI
+{
+    public static void main(String[] args) 
+    { 
+        //GameGUI gui = new GameGUI(true);
+        PlayerChoice plch = new PlayerChoice();
+    }
+}
